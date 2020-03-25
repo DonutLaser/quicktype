72847248f4b28817d65b48a0a602deedf037752b
DonutLaser <vidmantas.luneckas@gmail.com>
2020-03-25 22:19:07 +0200

    wpm component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
     <text-area></text-area>
+    <words-per-minute></words-per-minute>
   </div>
 </template>
 
 <script>
 	import TextArea from './components/TextArea.vue';
+  import WordsPerMinute from './components/WordsPerMinute.vue';
 
   export default {
     name: 'quicktype',
     components: {
       TextArea,
+      WordsPerMinute,
     }
   };
 </script>
 
-<style>
+<style scoped>
 #app {
   background-color: #141414;
   width: inherit;
---
+++ src/renderer/components/WordsPerMinute.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="words-per-minute">
+    77 WPM
+  </div>
+</template>
+
+<script>
+	export default {
+
+  };
+</script>
+
+<style scoped>
+	#words-per-minute {
+    color: #d9be2b;
+
+    width: fit-content;
+
+    position: absolute;
+    left: 50%;
+    top: 75%;
+    transform: translateX(-50%);
+
+
+    font-family: Arial;
+    font-weight: bold;
+    font-size: 3.3vw;
+  }
+</style>
