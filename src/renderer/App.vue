36e271038ab812e261fc05de15001be9cc6719f9
DonutLaser <vidmantas.luneckas@gmail.com>
2020-03-25 22:07:27 +0200

    Initial text area

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div id="app">
+    <text-area></text-area>
   </div>
 </template>
 
 <script>
+	import TextArea from './components/TextArea.vue';
+
   export default {
     name: 'quicktype',
+    components: {
+      TextArea,
+    }
   };
 </script>
+
+<style>
+#app {
+  background-color: #141414;
+  width: inherit;
+  height: inherit;
+}
+</style>
---
+++ src/renderer/components/TextArea.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="text-area">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate maximus congue. Donec a neque sed augue semper ultricies non nec elit. Nullam et magna ac ipsum pharetra sagittis. Etiam interdum congue neque, et tristique quam dapibus at. Donec eu consectetur lacus, luctus imperdiet purus. Pellentesque eros urna, dapibus vel consectetur ac, vestibulum vitae tellus. Phasellus nec pharetra lorem. Aliquam egestas turpis pharetra tellus rutrum fermentum ac a ex. Ut mauris neque, auctor sed commodo at, hendrerit vitae quam. Vivamus metus magna, rhoncus a turpis quis, pretium mattis sem. Maecenas ac mauris eu sem vehicula porttitor.
+	</div>
+</template>
+
+<script>
+	export default {
+
+	};
+</script>
+
+<style scoped>
+#text-area {
+	position: absolute;
+	left: 50%;
+	top: 10%;
+	transform: translateX(-50%);
+
+	width: 69%;
+	height: 62%;
+
+	font-family: consolas;
+	font-weight: bold;
+	font-size: 1.5vw;
+	line-height: 1.5;
+	color: #a4a4a4;
+	text-align: justify;
+	text-align-last: left;
+}
+</style>
