0bd44e23f972b089c2a2d469791e0feb98295e30
DonutLaser <vidmantas.luneckas@gmail.com>
2020-03-26 18:15:09 +0200

    Implemented word per minute calculations

--- src/renderer/components/WordsPerMinute.vue
+++ src/renderer/components/WordsPerMinute.vue
@@ -1,13 +1,46 @@
 <template>
   <div id="words-per-minute">
-    77 WPM
+    {{ wpm }} WPM
   </div>
 </template>
 
 <script>
-	export default {
+    export default {
+        props: [
+            'typed',
+            'enableTimer',
+        ],
+        watch: {
+            enableTimer: function(newValue) {
+                if (newValue) {
+                    const calculate = () => {
+                        const now = new Date().getTime();
+                        const end = now - this.startTime;
 
-  };
+                        this.endTimeInMin = (end / 1000) / 60;
+                    };
+
+                    calculate();
+
+                    this.startTime = new Date().getTime();
+                    this.timer = setInterval(calculate, 1000);
+                } else { clearInterval(this.timer); }
+            }
+        },
+        data: function () {
+            return {
+                startTime: 0,
+                endTimeInMin: 0.0,
+                timer: null,
+            };
+        },
+        computed: {
+            wpm: function () { 
+                if (this.typed === 0) { return 0; }
+                return Math.round((this.typed / 5) / this.endTimeInMin);
+            }
+        }
+    };
 </script>
 
 <style scoped>
