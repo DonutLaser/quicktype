a7e069c49971fc6e16ed932acd4812e66525b3e3
DonutLaser <vidmantas.luneckas@gmail.com>
2020-03-27 23:42:21 +0200

    Home page component

---
+++ src/renderer/components/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="home-page">
+		<div id="secondary-button-bar">
+			<settings-button></settings-button>
+			<help-button></help-button>
+		</div>
+	</div>
+</template>
+
+<script>
+	import HelpButton from './HelpButton.vue';
+	import SettingsButton from './SettingsButton.vue';
+
+	export default {
+		components: {
+			HelpButton,
+			SettingsButton,
+		},
+	};
+</script>
+
+<style scoped>
+	#secondary-button-bar {
+		position: absolute;
+		bottom: 30px;
+		right: 30px;
+		width: fit-content;
+	}
+</style>
