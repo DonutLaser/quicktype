589ce7e965932f82790eefc253e553323c74b6ba
DonutLaser <vidmantas.luneckas@gmail.com>
2020-03-27 23:41:25 +0200

    Create SecondaryButton.vue

---
+++ src/renderer/components/SecondaryButton.vue
@@ -0,0 +1,19 @@
+<template>
+	<base-button :icon="icon"></base-button>	
+</template>
+
+<script>
+	import BaseButton from './BaseButton.vue';
+	export default {
+		components: { BaseButton },
+		props: ['icon'],
+	};
+</script>
+
+<style scoped>
+#btn {
+	width: 2.5vw;
+	height: 2.5vw;
+	margin-left: 0.25vw;
+}
+</style>
