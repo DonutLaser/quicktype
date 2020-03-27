6138fb8cac270e35170917ce2bf75c3243290959
DonutLaser <vidmantas.luneckas@gmail.com>
2020-03-27 23:41:43 +0200

    Created settings and help buttons UI

---
+++ src/renderer/components/HelpButton.vue
@@ -0,0 +1,11 @@
+<template>
+	<secondary-button :icon="'question'"></secondary-button>
+</template>
+
+<script>
+	import SecondaryButton from './SecondaryButton.vue';
+
+	export default {
+		components: { SecondaryButton }
+	};
+</script>
---
+++ src/renderer/components/SettingsButton.vue
@@ -0,0 +1,11 @@
+<template>
+	<secondary-button :icon="'cog'"></secondary-button>
+</template>
+
+<script>
+	import SecondaryButton from './SecondaryButton.vue';
+
+	export default {
+		components: { SecondaryButton }
+	};
+</script>
