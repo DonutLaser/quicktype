200b037c871596f33f11cd3c46637428eab4f23e
DonutLaser <vidmantas.luneckas@gmail.com>
2020-03-27 23:42:04 +0200

    Moved textarea and wpm to home page component

---
+++ src/renderer/components/TypingPage.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="typing-page">
+		<text-area @type="onType($event)" @start="onStartPractice()" @end="onEndPractice()"></text-area>
+    	<words-per-minute :typed="typedCharacters" :enableTimer="isPracticing"></words-per-minute>  
+	</div>
+</template>
+
+<script>
+	import TextArea from './TextArea.vue';
+	import WordsPerMinute from './WordsPerMinute.vue';
+
+	export default {
+		components: {
+			TextArea,
+			WordsPerMinute,
+		},
+		data: function () {
+			return {
+				typedCharacters: 0,
+				isPracticing: false,
+			};
+		},
+		methods: {
+			onType: function (typed) {
+				this.typedCharacters = typed;
+			},
+			onStartPractice: function () {
+				this.isPracticing = true;
+			},
+			onEndPractice: function () {
+				this.isPracticing = false;
+			}
+		}
+	};
+</script>
