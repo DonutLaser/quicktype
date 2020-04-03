9b4b53f0024d79b80b9afdfa972783e59d0fe8cc
DonutLaser <vidmantas.luneckas@gmail.com>
2020-04-03 14:45:58 +0300

    Change random text for faster testing

--- src/mixins/randomText.js
+++ src/mixins/randomText.js
@@ -6,7 +6,7 @@ export default {
 	},
 	methods: {
 		getRandomText: function () {
-			this.currentText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate maximus congue. Donec a neque sed augue semper ultricies non nec elit. Nullam et magna ac ipsum pharetra sagittis. Etiam interdum congue neque, et tristique quam dapibus at. Donec eu consectetur lacus, luctus imperdiet purus. Pellentesque eros urna, dapibus vel consectetur ac, vestibulum vitae tellus. Phasellus nec pharetra lorem. Aliquam egestas turpis pharetra tellus rutrum fermentum ac a ex. Ut mauris neque, auctor sed commodo at, hendrerit vitae quam. Vivamus metus magna, rhoncus a turpis quis, pretium mattis sem. Maecenas ac mauris eu sem vehicula porttitor.';
+			this.currentText = 'Hello, darkness, my old friend';
 		},
 	},
 };
