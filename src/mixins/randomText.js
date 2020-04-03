b73cd7f77163392dd7ca5b0314d81d4aef5b5078
DonutLaser <vidmantas.luneckas@gmail.com>
2020-04-03 22:54:03 +0300

    Loading random article from Wikipedia

--- src/mixins/randomText.js
+++ src/mixins/randomText.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default {
 	data: function () {
 		return {
@@ -6,7 +8,16 @@ export default {
 	},
 	methods: {
 		getRandomText: function () {
-			this.currentText = 'Hello, darkness, my old friend';
+			let title = '';
+			axios.get('https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1&origin=*')
+				.then(resp => {
+					title = resp.data.query.random[0].title.split('/')[0];
+					axios.get('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exsentences=5&explaintext=1&origin=*&titles=' + encodeURIComponent(title))	
+						.then(resp => {
+							console.log(resp);
+							this.currentText = resp.data.query.pages[Object.keys(resp.data.query.pages)[0]].extract;
+						});
+				});
 		},
 	},
 };
