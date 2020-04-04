098ad43eef29b5e4163e8ad4b19cf534139a2240
DonutLaser <vidmantas.luneckas@gmail.com>
2020-04-04 14:21:21 +0300

    Using async/await with http requests

--- src/mixins/randomText.js
+++ src/mixins/randomText.js
@@ -8,16 +8,12 @@ export default {
 		};
 	},
 	methods: {
-		getRandomText: function () {
-			let title = '';
-			axios.get('https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1&origin=*')
-				.then(resp => {
-					title = resp.data.query.random[0].title.split('/')[0];
-					axios.get('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exsentences=5&explaintext=1&origin=*&titles=' + encodeURIComponent(title))	
-						.then(resp => {
-							this.currentText = asciifolder.foldReplacing(resp.data.query.pages[Object.keys(resp.data.query.pages)[0]].extract);
-						});
-				});
+		getRandomText: async function () {
+			const titleResp = await axios.get('https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1&origin=*');
+			const title = titleResp.data.query.random[0].title.split('/')[0]; // The title has several for some reason, e.g., Title1/Title2/Title3, so pick the first one
+
+			const textResp = await axios.get('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exsentences=5&explaintext=1&origin=*&titles=' + encodeURIComponent(title));
+			this.currentText = asciifolder.foldReplacing(textResp.data.query.pages[Object.keys(textResp.data.query.pages)[0]].extract).replace(/==|===/g, '~');
 		},
 	},
 };
